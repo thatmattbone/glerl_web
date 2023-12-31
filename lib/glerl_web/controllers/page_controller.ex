48b881465c23f3143747b10d495594c083aafc7e
Matt Bone <thatmattbone@gmail.com>
2023-12-31 00:58:11 -0600

    more work on a nice display of this data

--- lib/glerl_web/controllers/page_controller.ex
+++ lib/glerl_web/controllers/page_controller.ex
@@ -2,13 +2,12 @@ defmodule Glerl.Web.PageController do
   use Glerl.Web, :controller
 
   def current_conditions(conn, _params) do
-    latest_glerl_data =
-      Glerl.Realtime.Client.latest(30)  # two minute increments so this is an hour.
+    latest_glerl_data = Glerl.Realtime.Client.latest(30)  # two minute increments so this is an hour.
 
     conn
-    |> assign(:data, latest_glerl_data)
-    |> assign(:nav, :current_conditions)
-    |> render(:current_conditions)
+      |> assign(:data, latest_glerl_data)
+      |> assign(:nav, :current_conditions)
+      |> render(:current_conditions)
   end
 
   # def make_a_basic_point_plot() do
@@ -27,14 +26,14 @@ defmodule Glerl.Web.PageController do
 
   def previous_days(conn, _params) do
     conn
-    |> assign(:nav, :previous_days)
-    |> render(:previous_days)
+      |> assign(:nav, :previous_days)
+      |> render(:previous_days)
   end
 
   def historical_conditions(conn, _params) do
     conn
-    |> assign(:nav, :historical_conditions)
-    |> render(:historical_conditions)
+      |> assign(:nav, :historical_conditions)
+      |> render(:historical_conditions)
   end
 
   def about(conn, _params) do
--- lib/glerl_web/controllers/page_html.ex
+++ lib/glerl_web/controllers/page_html.ex
@@ -1,14 +1,16 @@
 defmodule Glerl.Web.PageHTML do
   use Glerl.Web, :html
 
-  alias Contex.{Dataset, Plot, PointPlot, LinePlot}
+  alias Contex.{Dataset, Plot, PointPlot, LinePlot, ContinuousLinearScale, Scale}
 
   embed_templates "page_html/*"
 
 
   attr :timestamp, :any, required: true
   def date_format(assigns) do
-    formatted_timestamp = Calendar.strftime(assigns.timestamp, "%m/%d %H:%M")
+    chicago_time = DateTime.shift_zone!(assigns.timestamp, "America/Chicago")
+
+    formatted_timestamp = Calendar.strftime(chicago_time, "%m/%d %H:%M")
 
     assigns = assigns |> assign(formatted_timestamp: formatted_timestamp)
 
@@ -17,16 +19,49 @@ defmodule Glerl.Web.PageHTML do
     """
   end
 
+  attr :temp_c, :float, required: true
+  def temp_format(assigns) do
+    fahrenheit = (assigns.temp_c * (9/5)) + 32
+
+    assigns = assigns |> assign(fahrenheit: fahrenheit)
+
+    ~H"""
+    <%= @fahrenheit %>
+    """
+  end
+  
+
+  attr :date, :any, required: true
   def point_plot(assigns) do
-    dataset = [{1, 50, 52}, {2, 20, 24}, {3, 30, 31}, {4, 5, 7}, {5, 10, 13}]
-      |> Dataset.new(["X", "Something", "Another Something"])
+    # IO.inspect(assigns.data)
+
+    dataset = for datapoint <- assigns.data do
+      chicago_time = DateTime.shift_zone!(datapoint.timestamp, "America/Chicago")
+
+      {
+        chicago_time,
+        datapoint.speed,
+        datapoint.gusts,
+        15,
+        20
+      }
+    end
+
+    # [{1, 50, 52}, {2, 20, 24}, {3, 30, 31}, {4, 5, 7}, {5, 10, 13}]
+    dataset = dataset
+      |> Dataset.new(["X", "Wind Speed", "Gusting To", "15", "20"])
+
+    y_scale = ContinuousLinearScale.new()
+      |> ContinuousLinearScale.domain(0.0, 30.0)
+      |> Scale.set_range(0.0, 30.0)
 
     options = [
-      mapping: %{x_col: "X", y_cols: ["Something", "Another Something"]}
+      mapping: %{x_col: "X", y_cols: ["Wind Speed", "Gusting To", "15", "20"]},
+      custom_y_scale:  y_scale
     ]
 
     plot = Plot.new(dataset, LinePlot, 900, 500, options)
-      |> Plot.titles("Sample Scatter Plot", nil)
+      # |> Plot.titles("Sample Scatter Plot", nil)
       |> Plot.plot_options(%{legend_setting: :legend_right})
 
     svg = Plot.to_svg(plot)
