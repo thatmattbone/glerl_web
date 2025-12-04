50eb1fcaff9afbd3b014d371fb84f3be67a7ace4
Matt Bone <thatmattbone@gmail.com>
2025-12-03 21:02:21 -0600

    new phoenix 1.8 app

---
+++ glerl_web/lib/glerl_web/application.ex
@@ -0,0 +1,33 @@
+defmodule Glerl.Web.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      Glerl.WebWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:glerl_web, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Glerl.Web.PubSub},
+      # Start a worker by calling: Glerl.Web.Worker.start_link(arg)
+      # {Glerl.Web.Worker, arg},
+      # Start to serve requests, typically the last entry
+      Glerl.WebWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Glerl.Web.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    Glerl.WebWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ glerl_web/lib/glerl_web_web/router.ex
@@ -0,0 +1,27 @@
+defmodule Glerl.WebWeb.Router do
+  use Glerl.WebWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {Glerl.WebWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Glerl.WebWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Glerl.WebWeb do
+  #   pipe_through :api
+  # end
+end
